f71f06ae59c3761995e51b3208e0aee5e8601fdf
AndrewsGabriel-2004 <andrewsgabriel205@gmail.com>
2025-02-17 20:26:17 -0300

    build: Tipos Primitivos e a Introdução do Curso + usando Intellij

---
+++ src/maratonajava/introducao/Aula01Introducao.java
@@ -0,0 +1,13 @@
+package maratonajava.introducao;
+
+public class Aula01Introducao {
+    public static void main(String[] args) {
+        //meu primeiro coment, use menos código
+
+        /*olá meu bom
+        isso é coment de 2 linhas */
+
+        System.out.println("Olá mundo!");
+
+    }
+}
---
+++ src/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+
+        // int,double,char,float,byte,short,long,boolean
+
+        System.out.println("Ola´");
+        boolean falso = false;
+        boolean verdadeiro = true;
+        short ageShort = 10;
+        byte agebyte = 10;
+        long numberLong = 100000;
+        float salaryFloat = 2500;
+        double salaryDouble = 2000;
+        int age = 10;
+        char caractere = 'M';
+
+        System.out.println("Andrews's age is " + age + " years old.");
+
+    }
+}
