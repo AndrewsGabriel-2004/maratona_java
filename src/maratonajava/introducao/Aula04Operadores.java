24fb3c0b9fec8b816cc6fa4b0eb5a369f99ba91d
AndrewsGabriel-2004 <andrewsgabriel205@gmail.com>
2025-02-19 14:55:57 -0300

    teste commit img.png

--- src/maratonajava/introducao/Aula04Operadores.java
+++ src/maratonajava/introducao/Aula04Operadores.java
@@ -10,7 +10,7 @@ public class Aula04Operadores {
 
         System.out.println(resultadoConta);
 
-        // % = resto
+        // % = rest
         int resto = 27 % 2;
         System.out.println(resto);
 
@@ -20,6 +20,35 @@ public class Aula04Operadores {
 
         boolean isTenEqualTen = 10 == 10;
         System.out.println(isTenEqualTen);
+
+        //&& = END // || = OR //  ! = NOT Logic Operators
+        double salary = 3500;
+        int age = 29;
+        boolean isRightWithLawGreaterThanThirty = age > 30 && salary >= 4612;
+        boolean isRightWithLawLessThanThirty = age < 30 && salary >= 3381;
+
+        System.out.println("isRightWithLawGreaterThanThirty " + isRightWithLawGreaterThanThirty);
+        System.out.println("isRightWithLawLessThanThirty " + isRightWithLawLessThanThirty);
+
+        double totalValueCheckingAccount = 200.0;
+        double totalValueSavingAccount = 10000.0;
+        double playStationValue = 5000.0;
+        boolean isPlayStation5purchasable = totalValueCheckingAccount > playStationValue || totalValueSavingAccount > playStationValue;
+
+        System.out.println("isPlayStation5purchasable " + isPlayStation5purchasable);
+
+        // == += -= *= /= %= Attributions Operators
+
+        double bonus = 1800.0;
+        bonus += 1000.0;
+        // bonus *= 2; bonus = 3600
+        // bonus %= 2; bonus = 0
+        // bonus++; bonus = 1801.0
+        // bonus--; bonus = 1799.0
+        System.out.println(bonus);
+
+        // bonus++ => firstly print bonus, and in the next line increase one more in bonus
+        // ++bonus => already increase one more in the print of bonus
 
     }
 }
---
+++ src/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,33 @@
+package maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+
+        int age = 14;
+
+        if (age >= 18) {
+
+            System.out.println("Pode botar para dentro mermão");
+        }
+        else {
+            System.out.println("Vira homem e dps vc bebe!");
+        }
+
+
+        boolean isAuthorizedtoBuyAlcoholicThings = age >= 18;
+        if (isAuthorizedtoBuyAlcoholicThings) {
+            System.out.println("Pode botar para dentro mermão");
+        }
+        if(!isAuthorizedtoBuyAlcoholicThings) {
+            System.out.println("Vira homem e dps vc bebe!");
+        }
+
+
+        
+
+
+
+
+
+    }
+}
