aa25f40994d84cbd031021f692e827eb3b00583c
AndrewsGabriel-2004 <andrewsgabriel205@gmail.com>
2025-05-13 20:15:55 -0300

    build: Fim de Optional

--- src/maratonajava/javacore/ZZDoptional/test/OptionalTest02.java
+++ src/maratonajava/javacore/ZZDoptional/test/OptionalTest02.java
@@ -2,12 +2,14 @@ package maratonajava.javacore.ZZDoptional.test;
 
 import maratonajava.javacore.ZZDoptional.domain.Manga;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.function.Supplier;
+import java.util.function.Predicate;
 
 public class OptionalTest02 {
     private static final List<Manga> mangas = new ArrayList<>(List.of(new Manga("Dragon Ball Z", 147L),
@@ -24,50 +26,39 @@ public class OptionalTest02 {
         String nameManga = sc.nextLine();
         System.out.print("Id:");
         Long id = sc.nextLong();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy 'T' HH:mm:ss");
         //1ºCreate a repository
         //2ºSearch a Manga by title, if exists update the title
         //3ºSearch the id, if not exists throw argumentException
         //4ºSearch for title, if not exist create a new manga
 
-        //2ºSearching process
-        Manga newManga = new Manga(nameManga, ThreadLocalRandom.current().nextLong(10000));
-        Optional<Manga> title = findTitle(newManga);
-        Manga verification = title.orElse(newManga);
-        if (!title.isPresent()){mangas.add(newManga);}
-        System.out.println(mangas);
-        System.out.println(verification);
+        /////////////////////////////////////////
 
-        System.out.println("================");
+        Optional<Manga> bySomething1 = findBySomething(m -> m.getTitle().equalsIgnoreCase(nameManga));
+        System.out.println(bySomething1.orElse(new Manga(nameManga, ThreadLocalRandom.current().nextLong(10000))));
+        if (bySomething1.isPresent()){
+            Manga manga = bySomething1.get();
+            manga.setTitle(manga.getTitle() + " Updated " + LocalDateTime.now().format(dateTimeFormatter));
+            System.out.println(mangas);
+        }
 
-        //3ºSearching the id
-        Optional<Manga> title2 = findId(new Manga(id));
-        title2.ifPresent(System.out::println);
+        Optional<Manga> bySomething2 = findBySomething(m -> m.getId().equals(id));
+        bySomething2.ifPresent(System.out::println);
         try {
-            title2.orElseThrow((Supplier<Throwable>) () -> new IllegalArgumentException("Don't exist this Id"));
+            bySomething2.orElseThrow(IllegalAccessException::new);
         } catch (Throwable e) {
             throw new RuntimeException(e);
         }
-
     }
 
-    private static Optional<Manga> findTitle(Manga obj) {
-        //1- Creating a repository with Mangas:
+    //Third option:functional program
+    private static Optional<Manga> findBySomething(Predicate<Manga> predicate) {
+        Manga found = null;
         for (Manga manga : mangas) {
-            if (manga.getTitle().equalsIgnoreCase(obj.getTitle())) {
-                manga.setTitle(obj.getTitle() + "  Updated");
-                return Optional.of(manga);
+            if (predicate.test(manga)) {
+                found = manga;
             }
         }
-        return Optional.empty();
-    }
-
-    private static Optional<Manga> findId(Manga obj){
-        //3- Searching process
-        for (Manga manga : mangas) {
-            if (manga.getId().equals(obj.getId())){
-                return Optional.of(manga);
-            }
-        }
-        return Optional.empty();
+        return Optional.ofNullable(found);
     }
 }
